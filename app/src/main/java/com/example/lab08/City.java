cf28bf5e27b4bb7b162ecd7d251a56439a9b5478
vedvikas-rgb <vedvikas@ualberta.ca>
2026-03-06 16:16:54 -0700

    301 starter Lab 8 TDD starter init TDD test

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -11,7 +11,7 @@ public class City {
 
     public String getCityName(){
         return this.city;
-    }
+    } //test comment
 
     public String getProvinceName(){
         return this.province;
